4fa6e660df6e3d743211f8d69c17011aaaa563aa
Fabricio Silva da Rocha <fsilvarocha@outlook.com>
2024-01-25 10:11:51 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { ResourceLoader } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'EstudoPromise';
+export class AppComponent implements OnInit {
+  
+  title = 'Promise';
+
+
+  myPromise(nome: string):Promise<string>{
+    return new Promise((resolve, reject)=>{
+      if(nome === 'Fabricio'){
+        setTimeout(() => {
+          resolve('Seja Bem Vindo '+nome);
+        }, 10000);
+      }
+      else{
+        setTimeout(() => {
+          reject('Ops! Você não é o Fabricio');  
+        }, 10000);
+        
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.myPromise('Fabricios')
+    .then(result=>console.log(result))
+    .catch(erro => console.log(erro))
+  }
 }
